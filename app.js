7b6b717cdd485ca26342316b0e76990e64969787
hibrid <samuelnmeje@gmail.com>
2020-03-23 13:21:29 +0400

    initial push

---
+++ app.js
@@ -0,0 +1,13 @@
+a = 5;
+b = 8;
+answer = 'hello developer';
+
+if (a > b) {
+    console.log(`${answer} what do you think`);
+
+} else if (b > a) {
+    console.log(`${answer} right`);
+} else {
+    console.log('final answer');
+
+}
